0f9f7148191b985785906901a516775fc1fc10bf
anilreddyperugu1 <peruguanilreddy6@gmail.com>
2025-11-02 20:05:55 +0530

    project completed

--- 04. Custom Image Classification using Transfer Learning/MobileNet_testing.py
+++ 04. Custom Image Classification using Transfer Learning/MobileNet_testing.py
@@ -1,15 +1,17 @@
+#Importing dependencies
 import numpy as np
 from keras.preprocessing import image
 from keras.models import load_model
 from keras.applications.mobilenet_v2 import preprocess_input
 
-
+#Loading the model
 model = load_model('model_mobilenet.keras')
 print("Model loaded!")
 
+#Represent Labels
 Labels = ["burger", "cookies", "donuts","french_fries","fried_chicken","fried_egg","hotdogs","pizza","steak","toast"]
 
-
+#Preprocess and input the test image
 img_path = 'test/pizza_2.jpg'
 img = image.load_img(img_path, target_size=(224, 224))
 img_array = image.img_to_array(img)
@@ -17,6 +19,7 @@ img_array = np.expand_dims(img_array, axis=0)
 img_array = preprocess_input(img_array)
 prediction = model.predict(img_array)
 
+#Prediction
 predicted_class = np.argmax(prediction[0])
 confidence = np.max(prediction[0])
 
