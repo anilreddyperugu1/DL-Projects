5ec04c634d4342853ae2411bf6baee84a23922cd
AnilReddy Perugu <peruguanilreddy6@gmail.com>
2025-12-29 15:43:49 +0530

    Delete 05. Image and Instance Segmentation directory

--- 05. Image and Instance Segmentation/Semantic_segmentation.py
+++
@@ -1,6 +0,0 @@
-import pixellib
-from pixellib import semantic
-
-segment_image = semantic()
-segment_image.load_pascalvoc_model('deeplabv3_xception_tf_dim_ordering_tf_kernels.h5')
-segment_image.segmentAsPascalVoc('01.jpg', output_image_name='semantic.jpg')
